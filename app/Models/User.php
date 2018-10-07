cde1ac91757d1355b79f095990c6ad8652148917
mohsenjalalian <jalalianmohsen@gmail.com>
2018-10-07 18:49:43 +0330

    user test last name

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ class User
 {
     public $first_name;
 
+    public $last_name;
+
     public function setFirstName($firstName)
     {
         $this->first_name = $firstName;
@@ -14,5 +16,20 @@ class User
     public function getFirstName()
     {
         return $this->first_name;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function getFullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
     }
 }
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -8,4 +8,20 @@ class UserTest extends \PHPUnit\Framework\TestCase
         $user->setFirstName('Billy');
         $this->assertEquals($user->getFirstName(), 'Billy');
     }
+
+    public function testThatWeCanGetTheLastName()
+    {
+        $user = new \App\Models\User;
+        $user->setLastName('Garrett');
+        $this->assertEquals($user->getLastName(), 'Garrett');
+    }
+
+    public function testFullNameIsReturned()
+    {
+        $user = new \App\Models\User;
+        $user->setFirstName('Billy');
+        $user->setLastName('Garrett');
+
+        $this->assertEquals($user->getFullName(), 'Billy Garrett');
+    }
 }
